9b2001abf1bf96475fe33c5cff3bfa532a3e6c3b
gnishida <gnishida@purdue.edu>
2015-07-21 12:02:03 -0400

    update door and window geometry.

--- SketchHouse/GLWidget3D.cpp
+++ SketchHouse/GLWidget3D.cpp
@@ -21,7 +21,7 @@ void Pen::setType(int type) {
 	}
 }
 
-GLWidget3D::GLWidget3D(QWidget *parent) : QGLWidget(QGLFormat(QGL::SampleBuffers), parent), lsystem(300, parametriclsystem::Literal("X", 0, -75, 0, 150)) {
+GLWidget3D::GLWidget3D(QWidget *parent) : QGLWidget(QGLFormat(QGL::SampleBuffers), parent), lsystem(300, parametriclsystem::Literal("X", 0, -75, 0, 150, 80)) {
 	mode = MODE_SKETCH;
 	dragging = false;
 	pen.setType(Pen::TYPE_WALL);
