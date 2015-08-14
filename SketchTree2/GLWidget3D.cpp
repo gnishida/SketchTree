65943b15a003ca4af2ee3f170a3529d393130e9c
gnishida <gnishida@purdue.edu>
2015-08-14 11:25:43 -0400

    Adding SketchTree2

---
+++ SketchTree2/GLWidget3D.cpp
@@ -0,0 +1,195 @@
+﻿#include "GLWidget3D.h"
+#include <iostream>
+#include "GLUtils.h"
+
+Pen::Pen() {
+	this->setType(TYPE_BRANCH);
+	this->setWidth(20);
+	this->setStyle(Qt::SolidLine);
+	this->setCapStyle(Qt::RoundCap);
+	this->setJoinStyle(Qt::RoundJoin);
+}
+
+void Pen::setType(int type) {
+	this->type = type;
+	if (type == TYPE_BRANCH) {
+		this->setColor(QColor(128, 0, 0, 255));
+	} else if (type == TYPE_LEAF) {
+		this->setColor(QColor(0, 255, 0, 255));
+	}
+}
+
+GLWidget3D::GLWidget3D(QWidget *parent) : QGLWidget(QGLFormat(QGL::SampleBuffers), parent), lsystem(parametriclsystem::Literal("X", 0, 25.0f, 0.0f)) {
+	mode = MODE_SKETCH;
+	dragging = false;
+	pen.setType(Pen::TYPE_BRANCH);
+	
+	// これがないと、QPainterによって、OpenGLによる描画がクリアされてしまう
+	setAutoFillBackground(false);
+
+	// 光源位置をセット
+	// ShadowMappingは平行光源を使っている。この位置から原点方向を平行光源の方向とする。
+	light_dir = glm::normalize(glm::vec3(-0.1, -0.2, -1));
+
+	// シャドウマップ用のmodel/view/projection行列を作成
+	glm::mat4 light_pMatrix = glm::ortho<float>(-200, 200, -200, 200, 0.1, 5000);
+	glm::mat4 light_mvMatrix = glm::lookAt(-light_dir * 200.0f, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	light_mvpMatrix = light_pMatrix * light_mvMatrix;
+}
+
+/**
+ * Draw the scene.
+ */
+void GLWidget3D::drawScene(int drawMode) {
+	if (drawMode == 0) {
+		glUniform1i(glGetUniformLocation(renderManager.program,"shadowState"), 1);
+	} else {
+		glUniform1i(glGetUniformLocation(renderManager.program,"shadowState"), 2);
+	}
+
+	renderManager.renderAll();
+}
+
+void GLWidget3D::drawLineTo(const QPoint &endPoint) {
+	QPoint pt1(lastPoint.x(), lastPoint.y());
+	QPoint pt2(endPoint.x(), endPoint.y());
+
+	QPainter painter(&sketch[pen.type]);
+	painter.setPen(pen);
+	painter.setRenderHint(QPainter::Antialiasing);
+	painter.setRenderHint(QPainter::HighQualityAntialiasing);
+
+	painter.drawLine(pt1, pt2);
+
+	lastPoint = endPoint;
+}
+
+void GLWidget3D::drawCircle(const QPoint &point) {
+	QPoint p(point.x(), point.y());
+
+	QPainter painter(&sketch[pen.type]);
+	painter.setPen(QPen(pen.color(), 1));
+	painter.setBrush(QBrush(pen.color()));
+	painter.setRenderHint(QPainter::Antialiasing);
+	painter.setRenderHint(QPainter::HighQualityAntialiasing);
+
+	painter.drawEllipse(p, (int)(pen.width() * 0.5), (int)(pen.width() * 0.5));
+}
+
+void GLWidget3D::resizeSketch(int width, int height) {
+	for (int i = 0; i < parametriclsystem::NUM_LAYERS; ++i) {
+		QImage newImage(width, height, QImage::Format_ARGB32);
+		newImage.fill(qRgba(255, 255, 255, 0));
+		QPainter painter(&newImage);
+
+		QImage img = sketch[i].scaled(width, height);
+		painter.drawImage(0, 0, img);
+		sketch[i] = newImage;
+	}
+}
+
+void GLWidget3D::resizeGL(int width, int height) {
+	// sketch imageを更新
+	resizeSketch(width, height);
+
+	// OpenGLの設定を更新
+	height = height ? height : 1;
+	glViewport(0, 0, width, height);
+	camera.updatePMatrix(width, height);
+}
+
+void GLWidget3D::mousePressEvent(QMouseEvent *e) {
+	if (mode == MODE_SKETCH) {
+		lastPoint = e->pos();
+		drawCircle(e->pos());
+		dragging = true;
+	} else {
+		camera.mousePress(e->x(), e->y());
+	}
+
+	update();
+}
+
+void GLWidget3D::mouseReleaseEvent(QMouseEvent *e) {
+	dragging = false;
+}
+
+void GLWidget3D::mouseMoveEvent(QMouseEvent *e) {
+	if (mode == MODE_SKETCH) {
+		drawLineTo(e->pos());
+	} else {
+		if (e->buttons() & Qt::LeftButton) { // Rotate
+			camera.rotate(e->x(), e->y());
+		} else if (e->buttons() & Qt::MidButton) { // Move
+			camera.move(e->x(), e->y());
+		} else if (e->buttons() & Qt::RightButton) { // Zoom
+			camera.zoom(e->x(), e->y());
+		}
+	}
+
+	update();
+}
+
+void GLWidget3D::initializeGL() {
+	renderManager.init(4096);
+
+	// 光源位置をセット
+	// ShadowMappingは平行光源を使っている。この位置から原点方向を平行光源の方向とする。
+	light_dir = glm::normalize(glm::vec3(-0.1, -0.2, -1));
+
+	// set the clear color for the screen
+	qglClearColor(QColor(224, 224, 224));
+
+	std::vector<Vertex> vertices;
+	glutils::drawAxes(1, 10, glm::mat4(), vertices);
+	renderManager.addObject("axis", "", vertices);
+
+	// sketch imageを初期化
+	for (int i = 0; i < parametriclsystem::NUM_LAYERS; ++i) {
+		sketch[i] = QImage(this->width(), this->height(), QImage::Format_ARGB32);
+	}
+}
+
+void GLWidget3D::paintEvent(QPaintEvent *event) {
+	// OpenGLで描画
+	makeCurrent();
+
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+
+	glUseProgram(renderManager.program);
+	renderManager.updateShadowMap(this, light_dir, light_mvpMatrix);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glEnable(GL_DEPTH_TEST);
+	glDisable(GL_TEXTURE_2D);
+
+	// Model view projection行列をシェーダに渡す
+	glUniformMatrix4fv(glGetUniformLocation(renderManager.program, "mvpMatrix"),  1, GL_FALSE, &camera.mvpMatrix[0][0]);
+	glUniformMatrix4fv(glGetUniformLocation(renderManager.program, "mvMatrix"),  1, GL_FALSE, &camera.mvMatrix[0][0]);
+
+	// pass the light direction to the shader
+	glUniform1fv(glGetUniformLocation(renderManager.program, "lightDir"), 3, &light_dir[0]);
+	
+	drawScene(0);
+
+	// OpenGLの設定を元に戻す
+	glShadeModel(GL_FLAT);
+	glDisable(GL_CULL_FACE);
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_LIGHTING);
+
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+
+	// QPainterで描画
+	QPainter painter(this);
+	painter.setOpacity(0.5);
+	for (int i = 0; i < parametriclsystem::NUM_LAYERS; ++i) {
+		painter.drawImage(0, 0, sketch[i]);
+	}
+	painter.end();
+
+	glEnable(GL_DEPTH_TEST);
+}
---
+++ SketchTree2/ParametricLSystem.h
@@ -0,0 +1,162 @@
+﻿#pragma once
+
+#include "RenderManager.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <vector>
+#include <map>
+#include "Vertex.h"
+#include "MyTimer.h"
+
+using namespace std;
+
+namespace parametriclsystem {
+
+const int NUM_LAYERS = 2;
+const int GRID_SIZE = 100;
+
+//const int MAX_ITERATIONS = 1000;//500;
+//const int MAX_ITERATIONS_FOR_MC = 10;
+//const int NUM_MONTE_CARLO_SAMPLING = 100;
+const double PARAM_EXPLORATION = 0.3;
+const double PARAM_EXPLORATION_VARIANCE = 0.1;
+const double LENGTH_ATTENUATION = 0.9;
+const double INITIAL_SIZE = 6.0;
+const double SIZE_ATTENUATION = 0.02;
+//const double MASK_RADIUS_RATIO = 0.11;
+
+class String;
+
+class Literal {
+public:
+	static enum { TYPE_TERMINAL = 0, TYPE_NONTERMINAL};
+
+public:
+	string name;
+	int depth;
+	double param_value1;
+	double param_value2;
+	bool param_defined;
+
+public:
+	Literal() : depth(0), param_value1(0.0), param_value2(0.0), param_defined(false) {}
+	Literal(const string& name, int depth, bool param_defined = false);
+	Literal(const string& name, int depth, double param_value);
+	Literal(const string& name, int depth, double param_value1, double param_value2);
+
+	String operator+(const Literal& l) const;
+	int type();
+};
+
+class Action;
+
+class String {
+public:
+	vector<Literal> str;
+	int cursor;				// expandするリテラルの位置
+
+public:
+	String();
+	String(const string& str, int depth);
+	String(const Literal& l);
+
+	int length() const { return str.size(); }
+	Literal operator[](int index) const { return str[index]; }
+	Literal& operator[](int index) { return str[index]; }
+	void operator+=(const Literal& l);
+	void operator+=(const String& str);
+	String operator+(const String& str) const;
+	void setValue(double value);
+	void replace(const String& str);
+
+	String getExpand() const;
+	void nextCursor(int depth);
+
+	void rewrite(const Action& action);
+};
+
+ostream& operator<<(ostream& os, const String& dt);
+
+/**
+ * 1ステップのderiveを表す。
+ */
+class Action {
+public:
+	static enum { ACTION_RULE = 0, ACTION_VALUE };
+
+public:
+	int type;		// 0 -- rule / 1 -- value
+	int index;	// actionsの中の何番目のactionか？
+	String rule;
+	double value;
+
+public:
+	Action() {}
+	Action(int index, const String& rule);
+	Action(int index, double value);
+};
+
+ostream& operator<<(ostream& os, const Action& a);
+
+/**
+ * MCTS用のノードクラス
+ */
+class Node {
+public:
+	double best_score;	// ベストスコア
+	std::vector<double> scores;
+	Node* parent;
+	std::vector<Node*> children;
+	std::vector<Action> actions;
+	std::vector<int> untriedActions;
+	Action action;
+	int visits;			// このノードの訪問回数
+	parametriclsystem::String model;
+	bool fixed;
+
+public:
+	Node(const String& model);
+	Node(const String& model, const Action& action);
+
+	Node* addChild(const String& model, const Action& action);
+	void setActions(std::vector<Action>& actions);
+	Action randomlySelectAction();
+	Node* UCTSelectChild();
+	Node* bestChild();
+};
+
+class ParametricLSystem {
+public:
+	int MAX_ITERATIONS;
+	int MAX_ITERATIONS_FOR_MC;
+	int NUM_MONTE_CARLO_SAMPLING;
+	double MASK_RADIUS_RATIO;
+
+	String axiom;
+	map<string, vector<Action> > actions_template;
+	MyTimer timer;
+
+public:
+	ParametricLSystem(const String& axiom);
+	void initActionsTemplate();
+	String derive(int random_seed);
+	String derive(const String& start_model, int max_iterations);
+	void draw(const String& model, RenderManager* renderManager);
+	void computeIndicator(const String& model, const glm::mat4& baseModelMat, std::vector<cv::Mat>& indicator);
+	void computeIndicator(const String& model, const glm::mat4& baseModelMat, const cv::Rect& roi, std::vector<cv::Mat>& indicator);
+	String inverse(const std::vector<cv::Mat>& target, const glm::mat4& mvpMat);
+	String UCT(const String& current_model, const std::vector<cv::Mat>& target, const glm::mat4& mvpMat, int derivation_step);
+	double score(const std::vector<cv::Mat>& indicator, const std::vector<cv::Mat>& target);
+
+private:
+	std::vector<Action> getActions(const String& model);
+	glm::vec2 computeCurrentPoint(const String& model, const glm::mat4& mvpMat, glm::mat4& modelMmat);
+	void releaseNodeMemory(Node* node);
+};
+
+float deg2rad(float deg);
+
+}
